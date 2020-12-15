5d1bac821c6756a72c4e0c21eeb6d3456b8f0541
Alexander S <alx.sherwin@gmail.com>
2020-12-15 17:54:40 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,1 @@
+puts "hello Alexander-Kun"
---
+++ practice.rb
@@ -0,0 +1,8 @@
+new_hash = {
+  :created => Time.now , 
+  :message => "Hello World"
+}
+# puts new_hash
+
+second_new_hash = Hash.new
+puts second_new_hash
